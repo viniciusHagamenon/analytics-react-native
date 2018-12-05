30ce46ecc6a50f220cb1b0575b8b37af66956719
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2018-12-05 17:26:15 +0100

    feat(ios): improve vanilla support

--- packages/test-app/test_ios.sh
+++ packages/test-app/test_ios.sh
@@ -2,11 +2,22 @@
 
 set -e
 
+cfg=""
+
 pushd project/ios
-    cp ../../Podfile .
-    yarn react-native link
-    pod install
+    if [[ $COCOAPODS == "yes" ]]; then
+        cp ../../Podfile .
+        yarn react-native link
+        pod install
+        cat ../requires.js >> ../App.tsx
+        cfg="cocoapods"
+    else
+        yarn remove $(cd ../../../integrations/build && echo @segment/*)
+        yarn add @segment/analytics-ios@github:segmentio/analytics-ios
+        yarn react-native link
+        cfg="vanilla"
+    fi
 popd
 
-yarn detox build --configuration ios
-yarn detox test --configuration ios
+yarn detox build --configuration ios-$cfg
+yarn detox test --configuration ios-$cfg
